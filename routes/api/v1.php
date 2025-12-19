34917be2137d1318746c0ec02509ea2c58ac8933
Victor Hugo Tello <victorhugotello@hotmail.com>
2025-12-19 12:42:43 -0600

    V1 optimized

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -3,16 +3,22 @@
 use App\Http\Controllers\AcabadoController;
 use App\Http\Controllers\ComponenteController;
 use App\Http\Controllers\HerrajeController;
-use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\ManoDeObraController;
+use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\ModulosController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\TipoDeMaterialController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('materiales', MaterialController::class)->parameters(['materiales' => 'material']);
-Route::apiResource('tipo-de-materiales', \App\Http\Controllers\TipoDeMaterialController::class)->parameters(['tipo-de-materiales' => 'tipoDeMaterial']);
-Route::apiResource('herrajes', HerrajeController::class);
-Route::apiResource('acabados', AcabadoController::class);
-Route::apiResource('mano-de-obras', ManoDeObraController::class);
-Route::apiResource('componentes', ComponenteController::class);
-Route::apiResource('modulos', ModulosController::class);
+Route::apiResource('materiales', MaterialController::class)
+    ->parameters(['materiales' => 'material']);
+
+Route::apiResource('tipo-de-materiales', TipoDeMaterialController::class)
+    ->parameters(['tipo-de-materiales' => 'tipoDeMaterial']);
+
+Route::apiResources([
+    'herrajes'      => HerrajeController::class,
+    'acabados'      => AcabadoController::class,
+    'mano-de-obras' => ManoDeObraController::class,
+    'componentes'   => ComponenteController::class,
+    'modulos'       => ModulosController::class,
+]);
