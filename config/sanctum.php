02baf8942de8e612fda4c464249b38d06e76a3f6
Victor Hugo Tello <victorhugotello@hotmail.com>
2025-12-19 21:18:22 -0600

    Connect with Lararel

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,9 +17,8 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:5173',
+        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
