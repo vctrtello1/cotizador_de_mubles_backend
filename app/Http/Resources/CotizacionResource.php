a9c93aa9abb37766a11fda1e3b558b14730f8a53
Victor Hugo Tello <victorhugotello@hotmail.com>
2025-12-21 12:18:40 -0600

    Cotizacion detallada fix

--- app/Http/Resources/CotizacionResource.php
+++ app/Http/Resources/CotizacionResource.php
@@ -20,7 +20,28 @@ class CotizacionResource extends JsonResource
             'cliente' => new ClienteResource($this->whenLoaded('cliente')),
             'fecha' => $this->fecha,
             'total' => $this->getRawOriginal('total'),
-            'detalles' => DetalleCotizacionResource::collection($this->whenLoaded('detalles')),
+            'modulos' => $this->whenLoaded('detalles', function () {
+                return $this->detalles->groupBy('modulo_id')->map(function ($detalles) {
+                    $modulo = $detalles->first()->modulo;
+                    return [
+                        'id' => $modulo->id,
+                        'nombre' => $modulo->nombre,
+                        'descripcion' => $modulo->descripcion,
+                        'codigo' => $modulo->codigo,
+                        'detalles' => $detalles->map(function ($detalle) {
+                            return [
+                                'id' => $detalle->id,
+                                'descripcion' => $detalle->descripcion,
+                                'cantidad' => $detalle->cantidad,
+                                'precio_unitario' => $detalle->precio_unitario,
+                                'subtotal' => $detalle->subtotal,
+                                'created_at' => $detalle->created_at,
+                                'updated_at' => $detalle->updated_at,
+                            ];
+                        })->values()
+                    ];
+                })->values();
+            }),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
