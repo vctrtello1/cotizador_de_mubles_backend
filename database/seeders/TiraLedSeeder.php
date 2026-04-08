339765ce3cd0ce353322c1c53a232d94e194a9f3
Victor Tello <vctrtello@icloud.com>
2026-04-07 18:44:30 -0600

    tiras led

--- database/seeders/TiraLedSeeder.php
+++ database/seeders/TiraLedSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\TiraLed;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TiraLedSeeder extends Seeder
